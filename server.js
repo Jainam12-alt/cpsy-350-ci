cea6f62c198a6c997107c6d65846ed542a0ec3f3
root <root@localhost.localdomain>
2025-04-08 17:08:44 -0600

    Initial commit: Set up Node.js app with GitHub Actions

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+ 
+app.get('/', (req, res) => {
+  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:000123456');
+});
+ 
+app.listen(port, () => {
+  console.log(`loading express\nRunning on http://0.0.0.0:${port}\n✔ responds to /\n❌ 404 everything else`);
+});
