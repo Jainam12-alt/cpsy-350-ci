8ae39018bf050650bb7246a06614831cac666cd8
root <root@localhost.localdomain>
2025-04-09 15:11:32 -0600

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
  
 app.get('/', (req, res) => {
-  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:00012346');
+  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:12346');
 });
  
 app.listen(port, () => {
