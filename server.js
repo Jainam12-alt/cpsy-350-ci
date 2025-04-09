5cbabc9ebadbb979233ac7353ad3619112d701b0
Jainam12-alt <jainampatel5052@gmail.com>
2025-04-09 17:31:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+'use strict';
+
 const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
 const app = express();
-const port = 3000;
- 
 app.get('/', (req, res) => {
-  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:0012346');
+  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:000123456');
 });
- 
-app.listen(port, () => {
-  console.log(`loading express\nRunning on http://0.0.0.0:${port}\n✔ responds to /\n❌ 404 everything else`);
-});
+
+const server = app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
+module.exports = server;
